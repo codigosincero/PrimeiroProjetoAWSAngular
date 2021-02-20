03a7d739a8abe0aca787f1427b5c8ce3e36b3dcf
codigosincero <codeasgamer@gmail.com>
2021-02-20 19:45:45 -0300

    correcao1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,22 @@ import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
-import { HeroesComponent } from "./heroes/heroes.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
 import { HeroDetailComponent } from "./hero-detail/hero-detail.component";
-import { HeroService } from "./hero.service";
+import { HeroesComponent } from "./heroes/heroes.component";
 import { MessagesComponent } from "./messages/messages.component";
-import { MessageService } from "./message.service";
+
 import { AppRoutingModule } from "./app-routing.module";
-import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule, AppRoutingModule],
   declarations: [
     AppComponent,
+    DashboardComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MessagesComponent,
-    DashboardComponent
+    MessagesComponent
   ],
-  bootstrap: [AppComponent],
-  providers: [HeroService, MessageService]
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
